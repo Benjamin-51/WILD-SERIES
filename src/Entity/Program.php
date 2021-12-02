1f9c2041af4e9695c28c0db04942753ce2cc538f
Benjamin-51 <babilliot.benjamin@gmail.com>
2021-12-02 12:24:56 +0100

    Quête 10-migration-update-program
    they have played us for absolute fools

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -45,6 +45,16 @@ class Program
      */
     private $seasons;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
     public function __construct()
     {
         $this->seasons = new ArrayCollection();
@@ -129,6 +139,30 @@ class Program
                 $season->setProgram(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
 
         return $this;
     }
